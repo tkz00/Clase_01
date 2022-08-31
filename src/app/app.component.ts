6cf13cc11e38ca7e266d57f63dac8314d759506b
tkz00 <theokatz@gmail.com>
2022-08-31 17:46:35 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Clase_01';
+
+  input_1 : number | null = null;
+  input_2 : number | null = null;
+
+  avg : number | null = null;
+  sum : number | null = null;
+
+  emptyInputs() : void {
+    this.input_1 = 0;
+    this.input_2 = 0;
+    this.avg = 0;
+    this.sum = 0;
+  }
+
+  getResults() : void {
+    if(this.input_1 != null && this.input_2 != null)
+    {
+      this.sum = (this.input_1 + this.input_2);
+      this.avg = (this.input_1 + this.input_2) / 2;
+    }
+  }
+
 }
